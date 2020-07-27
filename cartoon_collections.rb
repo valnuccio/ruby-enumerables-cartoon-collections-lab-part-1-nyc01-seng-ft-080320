eb41e55563b8cbfb94f3d69e872cee821ac52602
Valerie Nuccio <val.nuccio@gmail.com>
2020-07-27 18:03:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,7 @@
 def greet_characters(array)
+  array.each do |name|
+    puts "Hello #{name}!"
+  end
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
